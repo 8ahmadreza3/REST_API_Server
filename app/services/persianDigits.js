386d5b7622c6a08521e7133d7dd406b5f2ff3540
8ahmadreza3 <8arhp3@gmail.com>
2024-04-19 17:25:28 +0330

    persian digits

---
+++ app/services/persianDigits.js
@@ -0,0 +1,11 @@
+function toFarsiNumber (n) {
+  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+
+  return n
+    .toString()
+    .split('')
+    .map(x => farsiDigits[x])
+    .join('')
+}
+
+module.exports = toFarsiNumber
