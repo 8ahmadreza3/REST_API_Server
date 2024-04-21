701f3e7306f7627bb086d1a5321e8f1b795ba423
8ahmadreza3 <8arhp3@gmail.com>
2024-04-21 20:09:54 +0330

    signup user

---
+++ app/controllers/users/signup.js
@@ -0,0 +1,40 @@
+const UsersModel = require('../../models/usersModel')
+const hashServices = require('../../services/hashService')
+
+module.exports = async (req, res, next) => {
+  try {
+    const { name, userName, awsKey, phone, password } = req.body
+    if (!name || !userName || !phone || !password) {
+      res.send({
+        success: false,
+        message: 'Enter the information in full'
+      })
+    }
+    const same = await UsersModel.findOne({ $or: [{ phone }, { userName }] })
+    if (same) {
+      return res.send({
+        success: false,
+        message: 'try another phone number or username'
+      })
+    }
+
+    const hashPassword = hashServices.hashPassword(password)
+    const img = awsKey ? process.env.LIARA_URL + awsKey + '.png' : ''
+    const newUser = new UsersModel({
+      name,
+      userName: userName.replaceAll(' ', '_'),
+      phone,
+      isAdmin: false,
+      password: hashPassword,
+      img,
+      awsKey: awsKey || ''
+    })
+    await newUser.save()
+    res.status(201).send({
+      success: true,
+      message: 'new User added'
+    })
+  } catch (error) {
+    next(error)
+  }
+}
