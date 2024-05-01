d43f65c260c24e134dd1766928555748c36cc6f5
8ahmadreza3 <8arhp3@gmail.com>
2024-05-01 12:26:59 +0330

    auth rout

---
+++ app/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/auth')
+
+router.post('/token',  authController.authToken)
+
+module.exports = router
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 const products = require('./products')
 const upload = require('./upload')
 const users = require('./users')
+const auth = require('./auth')
 
 module.exports = (app) => {
   app.use('/products', products)
   app.use('/upload', upload)
   app.use('/users', users)
+  app.use('/auth', auth)
 }
