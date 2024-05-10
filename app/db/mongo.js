0167fc444f79817ab1f946a33a980c8315bb0761
8ahmadreza3 <8arhp3@gmail.com>
2024-05-10 11:59:27 +0330

    data base connection

--- app/db/mongo.js
+++ app/db/mongo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const { MONGO_DB, MONGO_HOST, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD } = process.env
+const { MONGO_DB, MONGO_HOST, MONGO_PORT } = process.env
 
 mongoose.connection.on('error', error => {
   console.log('mongodb connection failed', error.message)
 })
 const startMongoDB = () => {
-  mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin&replicaSet=rs0&directConnection=true`)
+  mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin&replicaSet=rs0&directConnection=true`)
     .then(() => {
       console.log('connected to mongodb')
     })
@@ -13,5 +13,8 @@ const startMongoDB = () => {
       console.log('could not connect to mongodb')
     })
 }
+
+// for connect with password url should be like :
+// mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}
 
 module.exports = startMongoDB
--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const fileUpload = require('express-fileupload')
 const express = require('express')
 
 module.exports = (app) => {
@@ -8,8 +7,4 @@ module.exports = (app) => {
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
   app.use(bodyParser.json())
-  app.use(fileUpload({
-    createParentPath: true,
-    useTempFile: true
-  }))
 }
