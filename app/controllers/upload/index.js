b898b91a01c6bfd6d9fb738e5f1d8501d6b8dfe7
8ahmadreza3 <8arhp3@gmail.com>
2024-05-10 12:05:20 +0330

    get public url

--- app/controllers/upload/index.js
+++ app/controllers/upload/index.js
@@ -2,25 +2,27 @@ const AWS = require('../../services/AWS')
 
 module.exports.uploadImg = async (req, res, next) => {
   try {
-    const imgFile = req.body
+    const imgFile = req.file
     if (!imgFile) {
       return res.send({
         success: false,
         message: 'The photo could not be uploaded'
       })
     }
-    const { response, awsKey } = await AWS.upload(imgFile)
+    const { response, awsKey } = await AWS.upload(imgFile.buffer)
     if (response.$metadata.httpStatusCode !== 200) {
       return res.send({
         success: false,
         message: 'The photo could not be uploaded'
       })
     }
+    const url = AWS.publicUrl(awsKey)
     res.send({
       success: true,
       message: 'Photo uploaded',
       data: {
-        awsKey
+        awsKey,
+        img: url
       }
     })
   } catch (error) {
