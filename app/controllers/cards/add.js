5026f5bf7fa9f5ce7d6a22ef407849b827667913
8ahmadreza3 <8arhp3@gmail.com>
2024-05-11 09:24:48 +0330

    add card

---
+++ app/controllers/cards/add.js
@@ -0,0 +1,37 @@
+const CardsModel = require('../../models/cardsModel')
+
+module.exports = async (req, res, next) => {
+  try {
+    const { product, user } = req.body
+    if (!user || !product) {
+      res.send({
+        success: false,
+        message: 'Enter the information in full'
+      })
+    }
+    const prevCard = await CardsModel.findOne({ user, product })
+    if (prevCard) {
+      const { n, nModefied } = await CardsModel.updateOne(prevCard, { count: prevCard.count + 1 })
+      if (!n || !nModefied) {
+        return res.send({
+          success: false,
+          message: "Couldn't update card"
+        })
+      }
+    } else {
+      const newCard = new CardsModel({
+        user,
+        product,
+        count: 1,
+        date: new Date()
+      })
+      await newCard.save()
+    }
+    res.status(201).send({
+      success: true,
+      message: 'added'
+    })
+  } catch (error) {
+    next(error)
+  }
+}
